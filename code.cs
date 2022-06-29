b09f3bffa1e02719138d68192077e0eda8ab5c22
root <69764760+root-7325@users.noreply.github.com>
2022-06-29 21:46:01 +0300

    Update code.cs
    gonvokod potuxonky fixitsa (no ne sovsem)

--- code.cs
+++ code.cs
@@ -1,17 +1,20 @@
 using System;
+using System.Collections.Generic;
 class Phone
 {
-    public int Number;
-    public string Name = "";
     public string Command;
 }
+
 class PhoneMenu
 {
     static void Main()
     {
+        Dictionary<string, string> phoneBook = new Dictionary<string, string>();
         int a1 = 0;
         Phone Menu = new Phone();
-        Console.WriteLine("            phone   \n");
+        var Call = "";
+        String Message = "";
+        Console.WriteLine("             phone   \n");
         Console.WriteLine("Write add to add new contact\n");
         Console.WriteLine("Write call to call someone\n");
         Console.WriteLine("Write sms to text someone\n");
@@ -23,24 +26,45 @@ class PhoneMenu
             {
                 case "add":
                     Contacts++;
-                    Phone Contact = new Phone();
+                    //Phone Contact = new Phone();
                     Console.WriteLine("Adding new contact...\n");
-                    Console.WriteLine("Write name of contact: ");
-                    Contact.Name = Console.ReadLine();
-                    Console.WriteLine("\nWrite number to contact in format +7<10 numbers next>: \n+7");
-                    Contact.Number = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Write name of contact & number in +7<10 numbers> format. ");
+                    phoneBook.Add(Console.ReadLine(), Console.ReadLine());
                     Console.WriteLine("Succesfully added new contact.\n");
                     break;
                 case "call":
                     Console.WriteLine("Choose contact or number to call\n");
-                    while (Contacts > 0)
+                    foreach (var Count in phoneBook)
                     {
-                        Console.WriteLine(Contact.Name + " " + Contact.Phone); //<----
-                        Contacts--;
+                        Console.WriteLine($"{Count.Key}");
+                        Call = Console.ReadLine();
+                        if (Call == Count.Key)
+                        {
+                            Console.WriteLine($"Calling {Count.Key}...");
+                            Console.WriteLine("No respond...");
+                            break;
+                        }
+                        Console.WriteLine("Calling " + Call);
+                        Console.WriteLine("No respond...");
                     }
                     break;
                 case "sms":
                     Console.WriteLine("Choose contact or number to send sms\n");
+                    foreach (var Count in phoneBook)
+                    {
+                        Console.WriteLine($"{Count.Key}");
+                        Call = Console.ReadLine();
+                        if (Call == Count.Key)
+                        {
+                            Console.WriteLine($"Type message for {Count.Key}:");
+                            Message = Console.ReadLine();
+                            Console.WriteLine("Message sent.");
+                            break;
+                        }
+                        break;
+                    }
+                    break;
+                case "":
                     break;
                 default:
                     Console.WriteLine("Uknown command!\n");
@@ -48,8 +72,6 @@ class PhoneMenu
 
             }
         } while (a1 == 0);
-
-        Console.ReadKey(true);
     }
 }
 
