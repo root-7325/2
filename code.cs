d879dec5a3540e2c5180b29686a68ff06e722d7c
root <69764760+root-7325@users.noreply.github.com>
2022-07-02 15:11:41 +0300

    Update code.cs

--- code.cs
+++ code.cs
@@ -5,8 +5,8 @@ class Phone
 {
     public string сommand;
     public static string probel;
-    public static string a1;
-    public static string a2;
+    public static string Name;
+    public static string Number;
     public static Dictionary<string, string> phoneBook = new Dictionary<string, string>();
     public static Dictionary<string, string> phoneSms = new Dictionary<string, string>();
 }
@@ -27,16 +27,16 @@ class PhoneMenu
     {
         Console.WriteLine("Adding new contact...\n");
         Console.WriteLine("Write name of contact & number in +7<10 numbers> format. ");
-        Phone.a1 = Console.ReadLine();
-        Phone.a2 = Console.ReadLine();
-        if (Phone.a2.All(char.IsDigit) == false || Phone.a2.Length > 9 || Phone.a2.Length < 9)
+        Phone.Name = Console.ReadLine();
+        Phone.Number = Console.ReadLine();
+        if (Phone.Number.All(char.IsDigit) == false || Phone.Number.Length > 9 || Phone.Number.Length < 9)
         {
                 Console.WriteLine("An error was made when entering the number!");
         }
 
         else
         {
-            Phone.phoneBook.Add(Phone.a1, Phone.a2);
+            Phone.phoneBook.Add(Phone.Name, Phone.Number);
             Console.WriteLine("Succesfully added new contact.\n");
         }
 
@@ -50,6 +50,7 @@ class PhoneMenu
             Console.WriteLine($"{Count.Key}");
             сall = Console.ReadLine();
 
+
             if (сall == Count.Key)
             {
                 Console.WriteLine($"Calling {Count.Key}...");
@@ -57,9 +58,22 @@ class PhoneMenu
                 break;
             }
 
+            if (сall == Count.Value)
+            {
+                Console.WriteLine($"Calling {Count.Key}...");
+                Console.WriteLine("No respond...");
+                break;
+            }
+
+            if (сall.All(char.IsDigit) == false || сall.Length > 9 || сall.Length < 9)
+            {
+                Console.WriteLine("An error was made when entering the number!");
+                break;
+            }
+
             else
             {
-                Console.WriteLine("Calling " + сall);
+                Console.WriteLine("Calling +7" + сall);
                 Console.WriteLine("No respond...");
                 break;
             }
@@ -76,6 +90,12 @@ class PhoneMenu
         {
             Console.WriteLine($"{Count.Key}");
             сall = Console.ReadLine();
+
+            if (сall.All(char.IsDigit) == false || сall.Length > 9 || сall.Length < 9)
+            {
+                Console.WriteLine("An error was made when entering the number!");
+                break;
+            }
 
             if (сall == Count.Key)
             {
@@ -101,7 +121,7 @@ class PhoneMenu
             else
             {
                 Console.WriteLine("Type message for +7" + сall);
-                Phone.phoneSms.Add(Console.ReadLine() + "+7" + сall, DateTime.Now.ToString("HH:mm:ss tt"));
+                Phone.phoneSms.Add(Console.ReadLine() + " - to +7" + сall, DateTime.Now.ToString("HH:mm:ss tt"));
                 Console.WriteLine("Message sent.");
             }
             break;
